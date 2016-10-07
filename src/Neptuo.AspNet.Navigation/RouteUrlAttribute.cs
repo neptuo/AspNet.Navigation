7a0573d2ef587b90f277caa362187ceff40c672f
maraf <mara@neptuo.com>
2016-10-07 16:36:20 +0200

    Add RouteUrlAttribute.

---
+++ src/Neptuo.AspNet.Navigation/RouteUrlAttribute.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Neptuo.AspNet.Navigation
+{
+    /// <summary>
+    /// Defines the route URL.
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Class)]
+    public class RouteUrlAttribute : Attribute
+    {
+        /// <summary>
+        /// Gets the route URL.
+        /// </summary>
+        public string Url { get; private set; }
+
+        /// <summary>
+        /// Creates new instance with <paramref name="url"/>.
+        /// </summary>
+        /// <param name="url">The route URL.</param>
+        public RouteUrlAttribute(string url)
+        {
+            if (String.IsNullOrEmpty(url))
+                throw new ArgumentNullException("url");
+
+            Url = url;
+        }
+    }
+}
