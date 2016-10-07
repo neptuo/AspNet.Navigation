475f736583b5b4f52178443e1b7ea4475f43300d
maraf <mara@neptuo.com>
2016-10-07 16:44:43 +0200

    Add IRouteDefaultProvider.

---
+++ src/Neptuo.AspNet.Navigation/IRouteDefaultProvider.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Neptuo.AspNet.Navigation
+{
+    /// <summary>
+    /// Defines the provider of route default values.
+    /// </summary>
+    public interface IRouteDefaultProvider
+    {
+        /// <summary>
+        /// Returns the enumeration of key-value pairs for the route defauls.
+        /// </summary>
+        /// <returns>The enumeration of key-value pairs for the route defauls.</returns>
+        IEnumerable<KeyValuePair<string, object>> GetKeyValues();
+    }
+}
---
+++ src/Neptuo.AspNet.Navigation/RouteDefaultAttribute.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Neptuo.AspNet.Navigation
+{
+    /// <summary>
+    /// Defines the default values for the route.
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Class, AllowMultiple = true, Inherited = true)]
+    public class RouteDefaultAttribute : Attribute, IRouteDefaultProvider
+    {
+        /// <summary>
+        /// Gets the key of the route values parameter.
+        /// </summary>
+        public string Key { get; private set; }
+
+        /// <summary>
+        /// Gets the value of the route values parameter.
+        /// </summary>
+        public object Value { get; private set; }
+
+        /// <summary>
+        /// Creates new instance with <paramref name="value"/> associated with <paramref name="key"/>.
+        /// </summary>
+        /// <param name="key">The key of the route values parameter.</param>
+        /// <param name="value">The value of the route values parameter.</param>
+        public RouteDefaultAttribute(string key, object value)
+        {
+            if (string.IsNullOrEmpty(key))
+                throw new ArgumentNullException("key");
+
+            Key = key;
+            Value = value;
+        }
+
+        public IEnumerable<KeyValuePair<string, object>> GetKeyValues()
+        {
+            return new List<KeyValuePair<string, object>>()
+            {
+                new KeyValuePair<string, object>(Key, Value)
+            };
+        }
+    }
+}
