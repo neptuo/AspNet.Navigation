8d26ab81862a9f3c0ddaccd2561e1fd7314cc366
maraf <mara@neptuo.com>
2016-10-07 16:11:33 +0200

    Base .NET-v4.6.1 project. Add RouteNameAttribute.

---
+++ src/Neptuo.AspNet.Navigation/RouteNameAttribute.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Neptuo.AspNet.Navigation
+{
+    /// <summary>
+    /// Defines the name of the route.
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Class)]
+    public class RouteNameAttribute : Attribute
+    {
+        /// <summary>
+        /// Gets the name of the route.
+        /// </summary>
+        public string Name { get; private set; }
+
+        /// <summary>
+        /// Creates new instance with implicit name taken by used convention.
+        /// </summary>
+        public RouteNameAttribute()
+        { }
+
+        /// <summary>
+        /// Creates new instance with explicit <paramref name="name"/>.
+        /// </summary>
+        /// <param name="name">The name of the route.</param>
+        public RouteNameAttribute(string name)
+        {
+            Name = name;
+        }
+    }
+}
