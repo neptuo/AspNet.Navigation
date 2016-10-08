48f8cc42c54e637b17855628af3a1512d9addd5b
maraf <mara@neptuo.com>
2016-10-08 14:21:33 +0200

    Add singleton, contract and default implementation of IRouteModelCollection.

---
+++ src/Neptuo.AspNet.Navigation/IRouteModelCollection.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Neptuo.AspNet.Navigation
+{
+    /// <summary>
+    /// Defines the collection of cached values created from the route models.
+    /// </summary>
+    public interface IRouteModelCollection
+    {
+    }
+}
---
+++ src/Neptuo.AspNet.Navigation/RouteModel.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Neptuo.AspNet.Navigation
+{
+    /// <summary>
+    /// The singleton class used in extension methods for registering routes, creating URLs and links.
+    /// </summary>
+    public static class RouteModel
+    {
+        private static IRouteModelCollection collection = new DefaultRouteModelCollection();
+
+        /// <summary>
+        /// Gets the singleton of the <see cref="IRouteModelCollection"/>.
+        /// </summary>
+        public static IRouteModelCollection Collection
+        {
+            get { return collection; }
+        }
+
+        /// <summary>
+        /// Sets the singleton of the <see cref="IRouteModelCollection"/>.
+        /// </summary>
+        /// <param name="collection">The instalce of collection to be used.</param>
+        /// <exception cref="ArgumentNullException">When the <paramref name="collection"/> is <c>null</c>.</exception>
+        public static void SetCollection(IRouteModelCollection collection)
+        {
+            if (collection == null)
+                throw new ArgumentNullException("collection");
+
+            RouteModel.collection = collection;
+        }
+    }
+}
