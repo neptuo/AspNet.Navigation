950976d24ca455fcec59ac4f28388bf6729c3b58
maraf <mara@neptuo.com>
2016-10-15 20:11:43 +0200

    Add provider of route constraints.

---
+++ src/Neptuo.AspNet.Navigation/Metadata/IRouteConstraintProvider.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Neptuo.AspNet.Navigation.Metadata
+{
+    /// <summary>
+    /// Defines the provider of route constraints.
+    /// </summary>
+    public interface IRouteConstraintProvider
+    {
+        /// <summary>
+        /// Returns the enumeration of key-value pairs for the route parameters constraints.
+        /// </summary>
+        /// <returns>The enumeration of key-value pairs for the route parameters constraints.</returns>
+        IEnumerable<KeyValuePair<string, object>> GetUrlConstraints();
+    }
+}
