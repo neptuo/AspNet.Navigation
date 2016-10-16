e565577dfe94aac25622f8f7a3a1a48a3c44c938
maraf <mara@neptuo.com>
2016-10-16 15:13:22 +0200

    Add VersioInfo. Consolidation of AssemblyInfo.

---
+++ src/Neptuo.AspNet.Navigation/Properties/VersionInfo.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Neptuo.AspNet.Navigation
+{
+    public static class VersionInfo
+    {
+        internal const string Version = "1.0.0";
+        internal const string Preview = "-beta1";
+
+        public static Version GetVersion()
+        {
+            return new Version(Version);
+        }
+    }
+}
