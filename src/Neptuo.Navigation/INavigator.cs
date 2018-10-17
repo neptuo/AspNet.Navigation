4fa9d67da030d6174039f60c5a4e07ce5e355ee3
Marek Fišera <mara@neptuo.com>
2018-10-17 19:00:26 +0200

    #1 - New solution and main project.

---
+++ src/Neptuo.Navigation/INavigator.cs
@@ -0,0 +1,20 @@
+﻿using Neptuo.Features;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Neptuo.Navigation
+{
+    /// <summary>
+    /// A navigation service facade.
+    /// </summary>
+    public interface INavigator
+    {
+        /// <summary>
+        /// Gets a collection of navigation features, a low-level contracts for executing navigation.
+        /// </summary>
+        IFeatureProvider Features { get; }
+    }
+}
