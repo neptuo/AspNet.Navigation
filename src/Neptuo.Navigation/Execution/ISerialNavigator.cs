d4f735b28bf4d131204b64dfb7608e0fd009eae3
Marek Fišera <mara@neptuo.com>
2018-10-17 19:07:59 +0200

    #1 - Add basic serial navigator.

---
+++ src/Neptuo.Navigation/Execution/ISerialNavigator.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Neptuo.Navigation.Execution
+{
+    /// <summary>
+    /// A basic navigator with capability to open view.
+    /// </summary>
+    public interface ISerialNavigator
+    {
+        /// <summary>
+        /// Opens a view paired to <paramref name="navigation"/> rule.
+        /// </summary>
+        /// <param name="navigation">A navigation rule.</param>
+        void Open(object navigation);
+    }
+}
---
+++ src/Neptuo.Navigation/NavigatorExtensions.cs
@@ -0,0 +1,24 @@
+﻿using Neptuo.Features;
+using Neptuo.Navigation.Execution;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Neptuo.Navigation
+{
+    /// <summary>
+    /// A basic navigation extensions.
+    /// </summary>
+    public static class NavigatorExtensions
+    {
+        /// <summary>
+        /// Opens a view paired to <paramref name="navigation"/> rule.
+        /// </summary>
+        /// <param name="navigator">A navigator.</param>
+        /// <param name="navigation">A navigation rule.</param>
+        public static void Open(this INavigator navigator, object navigation)
+            => navigator.Features.With<ISerialNavigator>().Open(navigation);
+    }
+}
