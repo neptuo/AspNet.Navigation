e84e4d3dc3bccf0225ed07590ffa14c82d921869
Marek Fišera <mara@neptuo.com>
2018-11-02 23:22:32 +0100

    #2 - Add Confirm rule in a new project Notifications.Callbacks.

---
+++ src/Neptuo.Navigation.Notifications.Callbacks/Rules/ConfirmButton.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Neptuo.Navigation.Rules
+{
+    /// <summary>
+    /// An enumeration of buttons for <see cref="Confirm"/>.
+    /// </summary>
+    public enum ConfirmButton
+    {
+        /// <summary>
+        /// A "Yes" or "No" buttons.
+        /// </summary>
+        YesNo,
+
+        /// <summary>
+        /// A three state "Yes", "No" or "Cancel" buttons.
+        /// </summary>
+        YesNoCancel
+    }
+}
---
+++ src/Neptuo.Navigation.Notifications.Callbacks/Rules/ConfirmResult.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Neptuo.Navigation.Rules
+{
+    /// <summary>
+    /// A result of <see cref="Confirm"/>.
+    /// </summary>
+    public enum ConfirmResult
+    {
+        /// <summary>
+        /// An "Yes" result.
+        /// </summary>
+        Yes,
+
+        /// <summary>
+        /// A "No" result.
+        /// </summary>
+        No,
+
+        /// <summary>
+        /// A "Cancel" result.
+        /// </summary>
+        Cancel
+    }
+}
