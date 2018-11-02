c2a26e1a51cec5edef5c6f7bf54a5744c4041b8f
Marek Fišera <mara@neptuo.com>
2018-11-02 23:32:07 +0100

    #3 - Add comments for IViewContext.

--- src/Neptuo.Navigation.Wpf/Views/IViewContext.cs
+++ src/Neptuo.Navigation.Wpf/Views/IViewContext.cs
@@ -6,8 +6,17 @@ using System.Threading.Tasks;
 
 namespace Neptuo.Navigation.Views
 {
+    /// <summary>
+    /// A context of an view.
+    /// Provides possibility to pass a result to the caller.
+    /// </summary>
+    /// <typeparam name="T">A type of the result.</typeparam>
     public interface IViewContext<T>
     {
+        /// <summary>
+        /// Closes current view and passes back <paramref name="result"/>.
+        /// </summary>
+        /// <param name="result">A view result.</param>
         void Close(T result);
     }
 }
