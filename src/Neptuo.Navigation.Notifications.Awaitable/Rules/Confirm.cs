b96c33b7ffad498a75b27723bf8b249e1f00e145
Marek Fišera <mara@neptuo.com>
2018-11-02 23:20:41 +0100

    #2 - Add Confirm rule in a new project Notifications.Awaitable.

---
+++ src/Neptuo.Navigation.Notifications.Awaitable/Rules/Confirm.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Neptuo.Navigation.Rules
+{
+    /// <summary>
+    /// A navigation rule asking an user a question.
+    /// </summary>
+    public class Confirm : IAsyncRule<ConfirmResult>
+    {
+        /// <summary>
+        /// Gets a notification title (not required).
+        /// </summary>
+        public string Title { get; }
+
+        /// <summary>
+        /// Gets a notification text.
+        /// </summary>
+        public string Text { get; }
+    }
+}
